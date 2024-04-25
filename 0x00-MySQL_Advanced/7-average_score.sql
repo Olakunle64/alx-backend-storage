ea3c88b680cc6c053a0bdc30e3ba1c4412555da3
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-25 23:41:40 +0100

    drop the procedure before creating it

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,20 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 
+-- drop the procedure if it exists
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 -- change the delimiter temporarily to $$
 DELIMITER $$
 
 -- create a procedure
-DELIMITER $$
-
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    -- Update the average score for the user
+    -- update the average_score of the user with the user_id
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (
+        -- calculate the average score of the user with the user_id
+        SELECT AVG(score) FROM corrections
+        WHERE user_id = user_id
+        )
     WHERE id = user_id;
 END$$
