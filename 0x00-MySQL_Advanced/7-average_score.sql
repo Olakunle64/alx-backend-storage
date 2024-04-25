b10abf9d478790d8146d65eecc69b86a3e034115
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-25 23:54:18 +0100

    correct task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,18 @@
 -- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
 
--- drop the procedure if it exists
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 -- change the delimiter temporarily to $$
 DELIMITER $$
 
 -- create a procedure
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_param INT)
 BEGIN
     -- update the average_score of the user with the user_id
     UPDATE users
     SET average_score = (
         -- calculate the average score of the user with the user_id
         SELECT AVG(score) FROM corrections
-        WHERE user_id = user_id
+        WHERE user_id = user_id_param
         )
-    WHERE id = user_id;
+    WHERE id = user_id_param;
 END$$
