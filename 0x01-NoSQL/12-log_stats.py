da0b3bd82be4e9790374f7296a06ba8968ab31e1
Olakunle64 <olakunleisiaq50@gmail.com>
2024-04-30 03:09:29 +0100

    debug task 6, 7 and 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,16 +5,17 @@
 from pymongo import MongoClient
 
 
-# connnect to the MongoDB server on a localhost address
-client = MongoClient()
-logs = client.logs
-nginx = logs.nginx
+if __name__ == "__main__":
+    # connnect to the MongoDB server on a localhost address
+    client = MongoClient()
+    logs = client.logs
+    nginx = logs.nginx
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("{} logs".format(nginx.estimated_document_count()))
-print("Methods:")
-for method in methods:
-    print("\tmethod GET: {}".format(
-        nginx.count_documents({"method": method}))
-        )
-print("{} status check".format(nginx.count_documents({"method": "GET", "path": "/status"})))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(nginx.estimated_document_count()))
+    print("Methods:")
+    for method in methods:
+        print("\tmethod GET: {}".format(
+            nginx.count_documents({"method": method}))
+            )
+    print("{} status check".format(nginx.count_documents({"method": "GET", "path": "/status"})))
