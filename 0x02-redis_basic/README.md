55bb250eb77cdb45a9e6b447d020235614c2efb8
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-01 17:29:08 +0100

    create the class Cache with only one method and the init method

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+<h3>Redis database</h3>
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This module has a class <Cache>"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache():
+    """A class with only one method."""
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, bytes, float, str]) -> str:
+        """method should generate a random key
+            (e.g. using uuid), store the input data
+            in Redis using the random key and return the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
