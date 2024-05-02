1755298d0b91f101747d4f9defe2ac7eb1400d65
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-02 02:36:04 +0100

    correct task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -47,16 +47,16 @@ def replay(method):
             )
 
 
-class Cache():
+class Cache:
     """A class with only one method."""
-    def __init__(self) -> None:
+    def __init__(self):
         """Constructor method."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     @count_calls
-    def store(self, data: Union[int, bytes, float, str]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """method should generate a random key
             (e.g. using uuid), store the input data
             in Redis using the random key and return the key
